573e8a3ca48bf47ad356398aebebc7bc4333c425
Jack Walton (PGR) <j.walton4@newcastle.ac.uk>
2018-03-28 13:14:30 +0100

    Update spell checker and documentation

--- inputs/spell_check.sh
+++ inputs/spell_check.sh
@@ -4,6 +4,9 @@
 # The spell-check checks the contents of commands such as \ref, \label, \cite,
 # etc. which almost never contain meaningful words.
 
+# To add words to dictionary append to inputs/custom_spellings
+# To add LaTex commands to ignore append to inputs/ignore_tex
+
 # Define escape codes for easier print formatting
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -17,6 +20,6 @@ printf "\n${RED}SPELL CHECK RESULTS${NC}\n\n"
 for file in chapters/*.tex
 do 
     printf "${GREEN}$file${NC}\n"
-    cat $file | aspell list -t --conf=$DIR/ignore_tex --add-extra-dicts=$DIR/ignore_spelling | sort | uniq
+    cat $file | aspell list -t --conf=$DIR/ignore_tex --add-extra-dicts=$DIR/custom_spellings | sort | uniq
     echo -e "\n"
 done
