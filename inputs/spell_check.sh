13b17f7026a7b224f36fc4187d75740e4f6fa5d4
Jack Walton (PGR) <j.walton4@newcastle.ac.uk>
2018-04-18 09:35:19 +0100

    Check all tex files, not just chapters

--- inputs/spell_check.sh
+++ inputs/spell_check.sh
@@ -7,19 +7,28 @@
 # To add words to dictionary append to inputs/custom_spellings
 # To add LaTex commands to ignore append to inputs/ignore_tex
 
-# Define escape codes for easier print formatting
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+# Use luxurious 256 colour
+function EXT_COLOUR () { echo -ne "\033[1;38;5;$1m"; }
+NO_COLOUR="\033[0m"
 
 # Directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Files to spell check
+files=$(find -type f -name "*.tex")
 
-printf "\n${RED}SPELL CHECK RESULTS${NC}\n\n"
-
-for file in chapters/*.tex
-do 
-    printf "${GREEN}$file${NC}\n"
-    cat $file | aspell list -t --conf=$DIR/ignore_tex --add-extra-dicts=$DIR/custom_spellings | sort | uniq
-    echo -e "\n"
+echo -e "\n`EXT_COLOUR 75`SPELL CHECK RESULTS$NO_COLOUR\n"
+for file in $files
+do
+    # List spelling mistakes
+    mistakes=$(cat $file | aspell list -t --conf=$DIR/ignore_tex \
+               --add-extra-dicts=$DIR/custom_spellings | sort | uniq)
+    # If there are no mistakes
+    if [ -z "$mistakes" ]
+    then
+        echo -e "`EXT_COLOUR 34`$file$NO_COLOUR"
+        echo -e "No spelling mistakes\n"
+    else
+        echo -e "`EXT_COLOUR 160`$file$NO_COLOUR"
+        echo -e "$mistakes\n"
+    fi
 done
