e94a908b4c52795de20e97842c258fd5d81fbf1e
Jack Walton <jwalton3141@gmail.com>
2020-03-18 16:13:04 +0000

    Tidy up spaghetti

--- fig/circ_stats_intro/atan_quadrants.py
+++ fig/circ_stats_intro/atan_quadrants.py
@@ -1,44 +1,98 @@
 #! /usr/bin/env python3
-""" Plot quadrants of R2 and the correction term which atan2 adds to atan. """
 
+"""Plot quadrants of R2 and the correction term which atan2 adds to atan."""
+
+import sys
+sys.path.insert(0, '..')
 import matplotlib.pyplot as plt
-from plot.pretty import set_size, savefig
 
-if __name__ == "__main__":
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.65, aspect='equal'))
+from pretty import set_size, savefig
 
+
+def main():
+    """Plot and save desired graph."""
+    fig, ax = plt.subplots(1, 1,
+            figsize=set_size(fraction=0.7, aspect='equal'))
+
+    add_axis(ax)
+
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+
+    shade_quadrants(ax)
+    annotate_quadrants(ax)
+
+    ax.set_aspect('equal')
+    savefig(fig, 'atan_quadrants')
+
+
+def add_axis(ax):
+    """Add x and y axis to figure."""
     # y axis
-    ax.arrow(0, -1, 0, 2, length_includes_head=True, width=0.005, head_width=0.04, color='k',
-             alpha=0.8)
+    ax.arrow(0, -1, 0, 2,
+            length_includes_head=True,
+            width=0.005,
+            head_width=0.04,
+            color='k',
+            alpha=0.8)
     # x axis
-    ax.arrow(-1, 0, 2, 0, length_includes_head=True, width=0.005, head_width=0.04, color='k',
-             alpha=0.8)
+    ax.arrow(-1, 0, 2, 0,
+            length_includes_head=True,
+            width=0.005,
+            head_width=0.04,
+            color='k',
+            alpha=0.8)
 
     # Remove ticks on axes
     ax.set_xticks([])
     ax.set_yticks([])
 
-    ax.set_xlim(-1, 1)
-    ax.set_ylim(-1, 1)
-
-    # Shade x > 0
-    ax.fill([0, 1, 1, 0], [-1, -1, 1, 1], 'C0', alpha=0.8)
-    # Shade x < 0, y > 0
-    ax.fill([0, -1, -1, 0], [0, 0, 1, 1], 'C1', alpha=0.8)
-    # Shade x < 0, y < 0
-    ax.fill([0, -1, -1, 0], [-1, -1, 0, 0], 'C2', alpha=0.8)
-
-    # Annotate quadrants
-    ax.text(0.5, 0.5, r'atan$(y/x)$', horizontalalignment='center', verticalalignment='center')
-    ax.text(0.5, -0.5, r'atan$(y/x)$', horizontalalignment='center', verticalalignment='center')
-    ax.text(-0.5, 0.5, r'atan$(y/x) + \pi$', horizontalalignment='center',
-            verticalalignment='center')
-    ax.text(-0.5, -0.5, r'atan$(y/x) - \pi$', horizontalalignment='center',
-            verticalalignment='center')
-
     # Add x and y axis labels
-    ax.text(0.9, 0.1, '$x$', horizontalalignment='center', verticalalignment='center')
-    ax.text(0.1, 0.9, '$y$', horizontalalignment='center', verticalalignment='center')
-    
-    ax.set_aspect('equal')
-    savefig('/data/thesis/fig/circ_stats_intro/', 'atan_quadrants')
+    ax.text(0.9, 0.1, '$x$',
+            horizontalalignment='center',
+            verticalalignment='center')
+    ax.text(0.1, 0.9, '$y$',
+            horizontalalignment='center',
+            verticalalignment='center')
+
+
+def shade_quadrants(ax):
+    """Shade the four quadrants according to how they are treated by atan2."""
+    # Shade x > 0
+    ax.fill([0, 1, 1, 0],
+            [-1, -1, 1, 1],
+            'C0', alpha=0.8)
+
+    # Shade x < 0, y > 0
+    ax.fill([0, -1, -1, 0],
+            [0, 0, 1, 1],
+            'C1', alpha=0.8)
+
+    # Shade x < 0, y < 0
+    ax.fill([0, -1, -1, 0],
+            [-1, -1, 0, 0],
+            'C2', alpha=0.8)
+
+
+def annotate_quadrants(ax):
+    """Add the formula for atan2 in each of the four quadrants."""
+    ax.text(0.5, 0.5, r'atan$(y/x)$',
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    ax.text(0.5, -0.5, r'atan$(y/x)$',
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    ax.text(-0.5, 0.5, r'atan$(y/x) + \pi$',
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    ax.text(-0.5, -0.5, r'atan$(y/x) - \pi$',
+            horizontalalignment='center',
+            verticalalignment='center')
+
+
+if __name__ == "__main__":
+    main()
+
--- fig/circ_stats_intro/average_demo.py
+++ fig/circ_stats_intro/average_demo.py
@@ -1,88 +1,110 @@
 #! /usr/bin/env python3
-""" Show why the arithmetic mean is inappropriate for circular data and demonstrate why the circular
- mean is better. 
-"""
 
+"""Illustrate the circular and arithmetic mean for data."""
+
+import sys
+sys.path.insert(0, '..')
 import matplotlib.pyplot as plt
 import matplotlib.transforms as mtransforms
 import numpy as np
-from plot.pretty import set_size
+
+from pretty import set_size, savefig
+
+
+def main():
+    """Plot and save desired graph."""
+    fig, ax = make_fig_ax()
+    fig, ax = plot_arith_mean(fig, ax)
+    savefig(fig, 'arith_mean', tight=None)
+
+    fig, ax = make_fig_ax()
+    plot_circ_mean(fig, ax)
+    savefig(fig, 'circ_mean', tight=None)
+
+
+def make_fig_ax():
+    """Make polar figure and axes to plot on."""
+    fig, ax = plt.subplots(1, 1,
+                           figsize=set_size(fraction=0.5, aspect='equal'),
+                           subplot_kw=dict(projection='polar'))
+
+    # Turn off radial grid
+    ax.grid(False)
+    # Remove radius marks
+    ax.set_yticks([])
+    # Only plot half of degree tick labels
+    ax.set_xticks(ax.get_xticks()[::2])
+
+    tick_labels = ax.get_xticklabels()
+    # Change 0 label to show 0 AND 360
+    tick_labels[0].set_text('$360^\\circ, 0^\\circ$')
+    ax.set_xticklabels(tick_labels)
+
+    return fig, ax
+
+
+def setup_plot(ax):
+    """Illustrations for the arithmetic mean and circular mean have the same setup."""
+    colour = 'C0'
+
+    ax.plot([0, 10 * np.pi / 180],
+            [0, 0.9],
+            c=colour,
+            lw=2)
+
+    ax.plot([0, 350 * np.pi / 180],
+            [0, 0.9],
+            c=colour,
+            lw=2)
+
+    add_arrow(ax, colour, 10)
+
+    add_arrow(ax, colour, 350)
+
+    return ax
+
+
+def add_arrow(ax, colour, angle):
+    # Bug in plotting arrows on polar plot. Work around is to plot at 0 and transform
+    ax.arrow(0, 0, 0, 1,
+             color=colour,
+             linewidth=1,
+             width=0.01,
+             length_includes_head=True,
+             transform=mtransforms.Affine2D().translate(angle * np.pi / 180, 0) + ax.transData,
+             head_width=0.08)
+
+
+def plot_arith_mean(fig, ax):
+    """Plot the arithmetic mean of 350 and 10."""
+    ax = setup_plot(ax)
+
+    colour = 'C2'
+    ax.plot([0, np.pi], [0, 0.9], c=colour, lw=2)
+    add_arrow(ax, colour, 180)
+    fig, ax = adjust_plots(fig, ax)
+    return fig, ax
+
+
+def plot_circ_mean(fig, ax):
+    """Plot the circular mean of 350 and 10."""
+    ax = setup_plot(ax)
+
+    colour = 'C1'
+    ax.plot([0, 0], [0, 0.9], c=colour, lw=2)
+    add_arrow(ax, colour, 0)
+    fig, ax = adjust_plots(fig, ax)
+    return fig, ax
+
+
+def adjust_plots(fig, ax):
+    """Make final adjustments to padding and limits of plots."""
+    ax.set_ylim([0, 1.05])
+    fig.tight_layout(pad=0.1)
+    fig.subplots_adjust(left=1 - fig.subplotpars.right)
+    return fig, ax
+
 
 if __name__ == "__main__":
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.45, aspect='equal'),
-                           subplot_kw=dict(projection='polar'))
-    # Turn of radial grid
-    ax.grid(False)
-    # Remove 'radius' marks
-    ax.set_yticks([])
-    # Only plot half of degree tick labels
-    ax.set_xticks(ax.get_xticks()[::2])
+    main()
 
-    # Hack hack hack
-    plt.pause(1)
-    tick_labels = ax.get_xticklabels()
-    # Change 0 label to show 0 AND 360
-    tick_labels[0].set_text('$360^\\circ, 0^\\circ$')
-    ax.set_xticklabels(tick_labels)
-
-    # Plot lines first to ensure uniform thickness of arrow tails
-    ax.plot([0, 10*np.pi/180], [0, 0.9], c='C0', lw=2)
-    ax.plot([0, 350*np.pi/180], [0, 0.9], c='C0', lw=2)
-    ax.plot([0, np.pi], [0, 0.9], c='C3', lw=2)
-
-    # Bug in plotting arrows on polar plot. Work around is to plot at 0 and transform
-    ax.arrow(0, 0, 0, 1, color='C0', linewidth=1, width=0.01, length_includes_head=True,
-         transform=mtransforms.Affine2D().translate(10*np.pi/180, 0) + ax.transData,
-         head_width=0.08)
-    
-    ax.arrow(0, 0, 0, 1, color='C0', linewidth=1, width=0.01, length_includes_head=True,
-         transform=mtransforms.Affine2D().translate(350*np.pi/180, 0) + ax.transData,
-         head_width=0.08)
-
-    ax.arrow(0, 0, 0, 1, color='C3', linewidth=1, width=0.01, length_includes_head=True,
-         transform=mtransforms.Affine2D().translate(np.pi, 0) + ax.transData,
-         head_width=0.08)
-
-    ax.set_ylim([0, 1.05])
-    fig.tight_layout(pad=0.1)
-    fig.subplots_adjust(left=1-fig.subplotpars.right)
-    fig.savefig('/data/thesis/fig/circ_stats_intro/arith_mean')
-
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.45, aspect='equal'),
-                           subplot_kw=dict(projection='polar'))
-    # Turn of radial grid
-    ax.grid(False)
-    # Remove 'radius' marks
-    ax.set_yticks([])
-    # Only plot half of degree tick labels
-    ax.set_xticks(ax.get_xticks()[::2])
-
-    # Hack hack hack
-    plt.pause(1)
-    tick_labels = ax.get_xticklabels()
-    # Change 0 label to show 0 AND 360
-    tick_labels[0].set_text('$360^\\circ, 0^\\circ$')
-    ax.set_xticklabels(tick_labels)
-
-    # Plot lines first to ensure uniform thickness of arrow tails
-    ax.plot([0, 10*np.pi/180], [0, 0.9], c='C0', lw=2)
-    ax.plot([0, 350*np.pi/180], [0, 0.9], c='C0', lw=2)
-    ax.plot([0, 0], [0, 0.9], c='C2', lw=2)
-
-    # Bug in plotting arrows on polar plot. Work around is to plot at 0 and transform
-    ax.arrow(0, 0, 0, 1, color='C0', linewidth=1, width=0.01, length_includes_head=True,
-         transform=mtransforms.Affine2D().translate(10*np.pi/180, 0) + ax.transData,
-         head_width=0.08)
-    
-    ax.arrow(0, 0, 0, 1, color='C0', linewidth=1, width=0.01, length_includes_head=True,
-         transform=mtransforms.Affine2D().translate(350*np.pi/180, 0) + ax.transData,
-         head_width=0.08)
-    
-    ax.arrow(0, 0, 0, 1, color='C2', linewidth=1, width=0.01, length_includes_head=True,
-         transform=mtransforms.Affine2D().translate(0, 0) + ax.transData,
-         head_width=0.08)
-
-    ax.set_ylim([0, 1.05])
-    fig.tight_layout(pad=0.1)
-    fig.subplots_adjust(left=1-fig.subplotpars.right)
-    fig.savefig('/data/thesis/fig/circ_stats_intro/circ_mean')
