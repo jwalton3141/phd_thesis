9606c6feb69709bf353b19dc7d50ab14406c65ec
Jack Walton <jwalton3141@gmail.com>
2020-03-19 15:48:46 +0000

    Include routine to create polar plot

--- fig/pretty.py
+++ fig/pretty.py
@@ -136,3 +136,57 @@ def adjust_spacing(fig, subplots=(1, 1), fraction=1):
                         hspace=0.4,
                         top=0.985)
 
+
+def rose_plot(ax, angles, bins=16, density=None, offset=0, lab_unit="degrees",
+              start_zero=False, **param_dict):
+    """
+    Plot polar histogram of angles on ax. ax must have been created using
+    subplot_kw=dict(projection='polar'). Angles are expected in radians.
+    """
+    # Wrap angles to [-pi, pi)
+    angles = (angles + np.pi) % (2*np.pi) - np.pi
+
+    # Set bins symetrically around zero
+    if start_zero:
+        # To have a bin edge at zero use an even number of bins
+        if bins % 2:
+            bins += 1
+        bins = np.linspace(-np.pi, np.pi, num=bins+1)
+
+    # Bin data and record counts
+    count, bin = np.histogram(angles, bins=bins)
+
+    # Compute width of each bin
+    widths = np.diff(bin)
+
+    # By default plot density (frequency potentially misleading)
+    if density is None or density is True:
+        # Area to assign each bin
+        area = count / angles.size
+        # Calculate corresponding bin radius
+        radius = (area / np.pi)**.5
+    else:
+        radius = count
+
+    # Plot data on ax
+    ax.bar(bin[:-1], radius, zorder=1, align='edge', width=widths,
+           edgecolor='C0', fill=False, linewidth=1)
+
+    # Set the direction of the zero angle
+    ax.set_theta_offset(offset)
+
+    # Remove ylabels, they are mostly obstructive and not informative
+    ax.set_yticks([])
+
+    if lab_unit == "radians":
+        tick_labels = [r'$0$',
+                       r'$\pi/4$',
+                       r'$\pi/2$',
+                       r'$3\pi/4$',
+                       r'$-\pi, \pi$',
+                       r'$-3\pi/4$',
+                       r'$-\pi/2$',
+                       r'$-\pi/4$']
+        ax.set_xticklabels(tick_labels)
+
+
