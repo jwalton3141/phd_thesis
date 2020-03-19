cf83dd2cbef129f11a70261a87e298e8812d34bf
Jack Walton <jwalton3141@gmail.com>
2020-03-19 15:48:12 +0000

    Simplify script

--- fig/circ_stats_intro/visualise_data.py
+++ fig/circ_stats_intro/visualise_data.py
@@ -1,58 +1,75 @@
 #! /usr/bin/env python3
 """ Compare visualisation of circular data with a histograms and rose plots. """
 
+import sys
+sys.path.insert(0, '..')
 import matplotlib.pyplot as plt
 import numpy as np
-from plot.pretty import set_size
-import plot.data as dplt
+
+from pretty import set_size, savefig, rose_plot
+
+
+def main():
+    """Produce histogram and polar histogram plots of random data."""
+    # Random uniform data
+    angles = np.random.uniform(-np.pi, np.pi, size=100)
+    plot_angles(angles, 'uniform')
+
+    # Random normal data
+    angles = np.random.normal(loc=0, scale=1, size=10000)
+    plot_angles(angles, 'normal')
+
+
+def plot_angles(angles, save_name):
+    """Plot and save histogram and polar histogram of angles."""
+    fig, ax = make_fig_ax()
+    plot_hist(fig, ax, angles)
+    savefig(fig, '{}_hist'.format(save_name), tight=None)
+
+    fig, ax = make_polar_fig_ax()
+    plot_polar_hist(fig, ax, angles)
+    savefig(fig, '{}_polar_hist'.format(save_name), tight=None)
+
+
+def make_fig_ax():
+    """Create empty figure and axes to plot regular histogram on."""
+    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.5))
+    return fig, ax
+
+
+def make_polar_fig_ax():
+    """Create empty figure and axes to plot polar histogram on."""
+    fig, ax = plt.subplots(1, 1,
+                           figsize=set_size(fraction=0.45, aspect='equal'),
+                           subplot_kw=dict(projection='polar'))
+    return fig, ax
+
+
+def plot_hist(fig, ax, angles):
+    """Produce a histogram plot angles on ax."""
+    ax.hist(angles, density=True, bins=16)
+
+    ax.set_xlabel(r'$\theta$')
+    ax.set_ylabel('density')
+
+    ax.set_yticks([])
+    ax.set_xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
+
+    ax.set_xticklabels([r'$-\pi$', r'$-\pi/2$', '$0$', r'$\pi/2$',r'$\pi$'])
+
+    ax.set_xlim([-np.pi, np.pi])
+
+    fig.tight_layout(pad=0.1)
+    fig.subplots_adjust(right=1-fig.subplotpars.left)
+
+
+def plot_polar_hist(fig, ax, angles):
+    """Produce a polar histogram plot of angles on ax."""
+    rose_plot(ax, angles, bins=16, start_zero=True, lab_unit='radians')
+    fig.tight_layout(pad=0.1)
+    fig.subplots_adjust(right=1-fig.subplotpars.left)
+
 
 if __name__ == "__main__":
-    # Generate random uniform data
-    angles = np.random.uniform(-np.pi, np.pi, size=100)
+    main()
 
-    # Plot data as traditional histogram
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.48))
-    ax.hist(angles, density=True, bins=16)
-    ax.set_xlabel(r'$\theta$')
-    ax.set_ylabel('density')
-    ax.set_yticks([])
-    ax.set_xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
-    ax.set_xticklabels([r'$-\pi$', r'$-\pi/2$', '$0$', r'$\pi/2$',r'$\pi$'])
-    ax.set_xlim([-np.pi, np.pi])
-    fig.tight_layout(pad=0.1)
-    # Pad left and right side equally so subcpation appears in centre of plot
-    fig.subplots_adjust(right=1-fig.subplotpars.left)
-    fig.savefig('/data/thesis/fig/circ_stats_intro/unif_angle_hist')
-
-    # Plot data on rose plot
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.45, aspect='equal'),
-                           subplot_kw=dict(projection='polar'))
-    dplt.rose_plot(ax, angles, bins=16)
-    fig.tight_layout(pad=0.1)
-    # Pad left and right side equally so subcpation appears in centre of plot
-    fig.subplots_adjust(right=1-fig.subplotpars.left)
-    fig.savefig('/data/thesis/fig/circ_stats_intro/unif_angle_rose')
-
-    # Generate random normal data
-    angles = np.random.normal(loc=0, scale=1, size=10000)
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.48))
-    ax.hist(angles, density=True, bins=20)
-    ax.set_xlabel(r'$\theta$')
-    ax.set_ylabel('density')
-    ax.set_yticks([])
-    ax.set_xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi])
-    ax.set_xticklabels([r'$-\pi$', r'$-\pi/2$', '$0$', r'$\pi/2$',r'$\pi$'])
-    ax.set_xlim([-np.pi, np.pi])
-    fig.tight_layout(pad=0.1)
-    # Pad left and right side equally so subcpation appears in centre of plot
-    fig.subplots_adjust(right=1-fig.subplotpars.left)
-    fig.savefig('/data/thesis/fig/circ_stats_intro/norm_angle_hist')
-
-    # Plot same data on a rose plot
-    fig, ax = plt.subplots(1, 1, figsize=set_size(fraction=0.45, aspect='equal'),
-                           subplot_kw=dict(projection='polar'))
-    dplt.rose_plot(ax, angles, bins=20)
-    fig.tight_layout(pad=0.1)
-    # Pad left and right side equally so subcpation appears in centre of plot
-    fig.subplots_adjust(right=1-fig.subplotpars.left)
-    fig.savefig('/data/thesis/fig/circ_stats_intro/norm_angle_rose')
