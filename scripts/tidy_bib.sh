f0f2e85a2f67c5fc835a6d8ea645b0263b029d91
Jack Walton <jwalton3141@gmail.com>
2020-06-30 19:16:18 +0100

    Change location of app
    Former-commit-id: 1b324255a4c683bd930be140c2a02d5d717f3a4d
    Former-commit-id: 72fd21feb8428ae54dfd98f8444104e584d96291

--- scripts/tidy_bib.sh
+++ scripts/tidy_bib.sh
@@ -7,6 +7,6 @@ cp references.bib references.bib.bak
 
 # -r points to biblatex stuff
 # -s sorts entries
-bibtool -r /data/apps/bibtool/bibtool-2.67/usr/share/bibtool/biblatex.rsc -s -i references.bib -o out.bib
+bibtool -r /usr/share/bibtool/biblatex.rsc -s -i references.bib -o out.bib
 
 mv out.bib references.bib
