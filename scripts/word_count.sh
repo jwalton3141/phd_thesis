a71349c6e3d2f394488242c853b136b68f7a25cd
Jack Walton <jwalton3141@gmail.com>
2020-07-06 13:16:59 +0100

    Break long lines
    Former-commit-id: 99109ab41b4fdf18f960e268873a5a146d78476c
    Former-commit-id: aba1a86af4489bd034ec581907cea64fe8955468

--- scripts/word_count.sh
+++ scripts/word_count.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
-COUNT=$(detex -l -e equation,table,algorithm,align,figure thesis.tex | tr '\n' ' ' | sed 's/*//g' | wc -w)
+
+# Strip tex code and count words
+COUNT=$(detex -l -e equation,table,algorithm,align,figure thesis.tex |
+        tr '\n' ' ' |
+        sed 's/*//g' |
+        wc -w)
+
 echo Word count: $COUNT
