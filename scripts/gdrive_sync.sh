aef60704b6916c046ecf8358fc7a4e9c365bc4b9
Jack Walton <jwalton3141@gmail.com>
2020-11-23 14:02:24 +0000

    update script
    Former-commit-id: 8c8d718c9322a62e9950c407b5a730cd05f2cf71
    Former-commit-id: b8831af6ed2d369205935d4d28f87544d6826106

--- scripts/gdrive_sync.sh
+++ scripts/gdrive_sync.sh
@@ -1,26 +1,25 @@
 #! /bin/bash
 
-#https://askubuntu.com/a/346580/1037399
+# Stick in crontab
+# I should capture exit code of clone, try handle errors etc.
+# but I'm trying hard not to procrastinate
+
+# Allow notify-send to appear in user session when executed with cron
+# https://askubuntu.com/a/346580/1037399
 eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
 
-
-# I have done zero testing of this script...
-# Stick in cron
-
+# Give user a heads up
 notify-send "cron: thesis sync started"
 
-# Move to thesis dir
-cd /home/jack/Documents/thesis
+# Work from /tmp/ to allow update to safely continue whilst I work
+rm -rf /tmp/thesis
+cp -r /home/jack/Documents/thesis /tmp/
+cd /tmp/thesis
 
 # Do a clean build
-make cleaner
-make
-
-# Copy build to /tmp/ so can continue working on thesis whilst uploading
-cp ~/Documents/thesis/thesis.pdf /tmp/
+make cleaner && make
 
 # Sync thesis to google drive
-rclone sync /tmp/thesis.pdf gdrive3141: -P
+rclone sync /tmp/thesis/thesis.pdf gdrive3141: -P
 
-# Something something should capture exit code and condition output but ...
 notify-send "cron: thesis sync finished"
