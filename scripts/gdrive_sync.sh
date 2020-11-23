e4c64b198fe18428b593e26db2913a0f73d06fc3
Jack Walton <jwalton3141@gmail.com>
2020-11-23 13:26:39 +0000

    nonsense
    Former-commit-id: ab89f45ba8cef89a709916f6bede9dc987def540
    Former-commit-id: c1508bfca94085e73abaef53bc849905a4b05f67

--- scripts/gdrive_sync.sh
+++ scripts/gdrive_sync.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
 
+# I have done zero testing of this script...
+# Stick in cron
+
+notify-send "cron: thesis sync started"
+
+# Move to thesis dir
 cd /home/jack/Documents/thesis
+
+# Do a clean build
 make cleaner
 make
 
 # Copy build to /tmp/ so can continue working on thesis whilst uploading
 cp ~/Documents/thesis/thesis.pdf /tmp/
 
+# Sync thesis to google drive
 rclone sync /tmp/thesis.pdf gdrive3141: -P
+
+# Something something should capture exit code and condition output but ...
+notify-send "cron: thesis sync finished"
