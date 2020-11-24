40d0e28b0d3c6fb3388a6a33e9edf31896ce749c
Jack Walton <jwalton3141@gmail.com>
2020-11-24 18:11:16 +0000

    Capitals are importantest
    Former-commit-id: 64f992b804f33f0b7010cf6a886dbe59a5a4d8c0
    Former-commit-id: 022bbebedc8f6888fdb0b1a598cb74c4cb374cf8

--- scripts/gdrive_sync.sh
+++ scripts/gdrive_sync.sh
@@ -6,7 +6,7 @@
 
 # Give user a heads up
 # https://stackoverflow.com/a/53598510/11021886
-XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Cron: "thesis sync started"
+XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Cron: "Thesis sync started"
 
 # Work from /tmp/ to allow update to safely continue whilst I work
 rm -rf /tmp/thesis
@@ -19,4 +19,4 @@ make cleaner && make
 # Sync thesis to google drive
 rclone sync /tmp/thesis/thesis.pdf gdrive3141: -P
 
-XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Cron: "thesis sync finished"
+XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Cron: "Thesis sync finished"
