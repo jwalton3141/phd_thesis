e23b279c974365810bad420330bbecb83437bfb8
Jack Walton <jwalton3141@gmail.com>
2020-11-25 10:18:35 +0000

    don't sync if build fails. notify user of failing builds
    Former-commit-id: 1afcfaf8e3e2e1a86e7f60304af043912577e929
    Former-commit-id: 18d70232c40f084326e2431269032cc9e721d3b9

--- scripts/gdrive_sync.sh
+++ scripts/gdrive_sync.sh
@@ -1,22 +1,37 @@
 #! /bin/bash
 
-# Stick in crontab
-# I should capture exit code of clone, try handle errors etc.
-# but I'm trying hard not to procrastinate
+# This script creates a clean build of my thesis and syncs the pdf with google
+# drive. With this my supervisors can always have the latest build of my
+# thesis, without having to make it themselves.
 
-# Give user a heads up
+# The intention is for this script to be executed regularly with cron.
+
 # https://stackoverflow.com/a/53598510/11021886
-XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Cron: "Thesis sync started"
+export XDG_RUNTIME_DIR=/run/user/$(id -u) 
 
-# Work from /tmp/ to allow update to safely continue whilst I work
+notify-send Cron: "Thesis build & sync started"
+
+# Work from /tmp/ so I can continue to work during sync
 rm -rf /tmp/thesis
-cp -r /home/jack/Documents/thesis /tmp/
+cp -r $HOME/Documents/thesis /tmp/
 cd /tmp/thesis
 
 # Do a clean build
 make cleaner && make
 
-# Sync thesis to google drive
-rclone sync /tmp/thesis/thesis.pdf gdrive3141: -P
+if [ $? -eq 0 ]
+then
+    notify-send Cron: "Thesis build completed successfully"
 
-XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send Cron: "Thesis sync finished"
+    # Sync thesis to google drive
+    rclone sync /tmp/thesis/thesis.pdf gdrive3141: -P \
+        && notify-send Cron: "Thesis sync completed successfully" \
+        || notify-send Cron: "Thesis sync failed"
+
+    # Clean up after succesful build
+    rm -rf /tmp/thesis
+else
+    notify-send Cron: "Thesis build failed"
+    # Don't clean up after build fail (logs and output useful for debug)
+fi
+
