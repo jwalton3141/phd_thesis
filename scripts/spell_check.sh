477e73a9643c2f36d4aeece36f58fd8d3d1a314d
Jack Walton <jwalton3141@gmail.com>
2020-11-26 16:06:04 +0000

    Minor fixed and line breaks
    Former-commit-id: 60bf79ed5c7c4bdd7969fbe9098a2df3521d4d38
    Former-commit-id: c19b4ce148929776f9dac04443c8648a9a9879e0

--- scripts/spell_check.sh
+++ scripts/spell_check.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# The inbuilt spell-check of TexMaker leaves much to be desired.
-# The spell-check checks the contents of commands such as \ref, \label, \cite,
-# etc. which almost never contain meaningful words.
-
-# To add words to dictionary append to inputs/custom_spellings
-# To add LaTex commands to ignore append to inputs/ignore_tex
+# To add words to dictionary append to internal/custom_spellings
+# To add LaTex commands to ignore append to internal/ignore_tex
 
 # Use luxurious 256 colour
 function EXT_COLOUR () { echo -ne "\033[1;38;5;$1m"; }
@@ -13,15 +9,20 @@ NO_COLOUR="\033[0m"
 
 # Directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# Spell check all chapters, dedication, appendix etc.
+
+# Spell check all .tex files in chapters/ directory
 files=$(find chapters/ . -maxdepth 1 -type f -name "*.tex")
 
 echo -e "\n`EXT_COLOUR 75`SPELL CHECK RESULTS$NO_COLOUR\n"
+
 for file in $files
 do
     # List spelling mistakes
-    mistakes=$(cat $file | aspell list -t --conf=$DIR/ignore_tex \
-               --add-extra-dicts=$DIR/custom_spellings | sort | uniq)
+    mistakes=$(cat $file | 
+               aspell list -t --conf=$DIR/../internal/ignore_tex \
+                              --add-extra-dicts=$DIR/../internal/custom_spellings | \
+               sort | uniq)
+
     # If there are no mistakes
     if [ -z "$mistakes" ]
     then
